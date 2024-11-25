186905008f51393635c30731fe84ada00124fe4f
unknown <javiervillarroel117@gmail.com>
2024-11-25 11:12:09 -0500

    don pendejo

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  private http=inject(HttpClient)
+
+  loginForm=new FormGroup({
+    email:new FormControl("",[Validators.email, Validators.required]),
+    password: new FormControl("", Validators.required)
+  })
+
+  funIngresar(){
+    this.http.get("http://jsonlaceholder.typicode.com/todos/1").subscribe(
+      (res)=>{
+        console.log(res)
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
+    alert("Ingresando...")
+  }
+
+}
